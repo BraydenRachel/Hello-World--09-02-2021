c59bd55b6f0916c7538675675c318640ecd2b809
Brayden Rachel <86629850+BraydenRachel@users.noreply.github.com>
2021-09-02 15:49:44 -0500

    Add files via upload

---
+++ Hello_World.py
@@ -0,0 +1,14 @@
+print("Hello World")
+
+name = "Brayden!"
+print("Hello", name)
+print("Hello "+ name)
+
+number = "12"
+print("Hello", number)
+print("Hello " + number)
+
+fav_food1 = "sushi"
+fav_food2 = "chicken"
+print(f"I love to eat {fav_food1} and {fav_food2}!")
+print("I love to eat {} and {}!".format(fav_food1, fav_food2))
